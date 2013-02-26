dc18ead4e0902440ea011631c61fe73512657305
david owen greenberg <owen.david.us@gmail.com>
2013-02-26 18:48:40 -0500

    Improved page content bean, so you can access controller vars using $this->name just like in the layout

--- x_objects/classes/webobjects/xo_page_content.php
+++ x_objects/classes/webobjects/xo_page_content.php
@@ -4,14 +4,21 @@
  * Date: 06/02/13
  * Time: 11:59 AM
  */
-class xo_page_content {
+class xo_page_content extends magic_object {
     private $key = "home";
-    public function __construct($key){
+    private $vars = array();
+    public function __construct($key,$vars = array()){
         $this->key = $key;
+        $this->vars = $vars;
+        foreach( $vars as $name =>$value)
+            $this->$name = $value;
     }
     public function __toString(){
         try {
-            $str = x_object::create($this->key)->html($this);
+            $xobj =x_object::create($this->key);
+            foreach ( $this->vars as $name=>$value)
+                $xobj->$name = $value;
+            $str = $xobj->html($this);
         } catch (Exception $e){
             $str = "<div style='background-color:#1e1e1e;color: white;width: 800px;height:auto;min-height: 100px;margin: 50px auto 0;padding: 10px;font-size: 25pt'><p>X-Objects says:</p><p>".$e->getMessage()."</p></div>";
         }
