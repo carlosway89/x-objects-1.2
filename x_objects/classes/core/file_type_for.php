545c1f707e4801463d66268d1187fa2a8dc6c772
x-objects <david@reality-magic.com>
2013-03-31 12:54:12 -0500

    Added a new printable component to quickly given the canonical file type ("video","image" et al) for a given filename, based on its extension

---
+++ x_objects/classes/core/file_type_for.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Component to get a generic "file type" name for a given filename
+ *
+ * User: "David Owen Greenberg" <owen.david.us@gmail.com>
+ * Date: 12/02/13
+ * Time: 05:05 PM
+ */
+class file_type_for {
+    private $extension = "";
+    private $type = 'unknown';
+    private $translations = array(
+        'video'=>array('avi','wmv','mov','mpg','mpeg','mp4','flv'),
+        'document'=>array('pdf','xls','doc','ppt','txt','rtf','xlsx','docx','pptx'),
+        'image'=>array('jpg','jpeg','gif','png','bmp','tif','tga'),
+        'audio'=>array('mp3','wma','wav','m4a','flac','aiff','aif','snd','wv','ape','aac')
+    );
+    public function __construct($filename){
+        if ( preg_match( '/(.+)\.([a-z|A-Z|0-9|_]+)/',$filename,$hits)){
+            $this->extension = $hits[2];
+            if ( $this->extension){
+                foreach( $this->translations as $type =>$extensions){
+                    if ( in_array($this->extension,$extensions)){
+                        $this->type = $type;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    public function __toString(){
+        return $this->type;
+    }
+}
