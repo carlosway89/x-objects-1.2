ab4950fd15558840edc43b851c8b270de698df1b
x-objects <david@reality-magic.com>
2013-04-28 10:24:11 -0500

    Tweaks and improvements for API success messages

---
+++ x_objects/classes/core/xo_text_filter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * Abstract representation of a text filter
+ *
+ * User: "David Owen Greenberg" <david@reality-magic.com>
+ * Date: 28/04/13
+ * Time: 09:39 AM
+ */
+
+abstract class xo_text_filter {
+    protected $text = null;
+    public function __toString(){
+        if ( $this->text === null) user_error(get_called_class().": text was not set by child class",E_USER_WARNING);
+        return $this->text;
+    }
+}
