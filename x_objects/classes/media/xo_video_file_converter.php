90112bc5a481fd2eabf083434b7221846b728dff
x-objects <david@reality-magic.com>
2013-05-02 13:35:43 -0500

    support for MP4 to ogg conversion

--- x_objects/classes/media/xo_video_file_converter.php
+++ x_objects/classes/media/xo_video_file_converter.php
@@ -16,7 +16,7 @@ class xo_video_file_converter {
     private $extension = '';
     public $error = '';
     public $output = '';
-    private $extensions = array('avi','wmv','mov');
+    private $extensions = array('avi','wmv','mov','mp4');
     public $command = '';
     public function __construct($filename){
         global $container;
@@ -32,15 +32,22 @@ class xo_video_file_converter {
         else {
             // convert from AVI
             if ( in_array($this->extension,$this->extensions)){
-                $this->new_filename = preg_replace("/\.$this->extension/",'.mp4',$this->filename);
+                // support to convert MP4 to Ogg
+                $new_extension = $this->extension == 'mp4'?'.ogg':'.mp4';
+                $this->new_filename = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
                 $config = $this->container->config->ffmpeg;
                 if ( ! $config )
                     $this->error = 'There is no X-Objects configuration for ffmpeg';
                 else {
                     $ext = $this->container->platform() == 'win'?".exe":'';
-                    $cname = "ffmpeg$ext";
+                    $cname = $this->extension == 'mp4'? 'ffmpeg2theora': "ffmpeg$ext";
                     $cmd = (string) $config->directory . ''.$cname.' -i '.$this->filename.
                         ' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline '. $this->new_filename . ' 2>&1 ';
+                    // if ogg...
+                    if ($this->extension == 'mp4'){
+                        $new_filename = preg_replace('/\.mp4/','.ogg',$this->filename);
+                        $cmd = "$cname $this->filename $new_filename 2>&1";
+                    }
                     $this->command = $cmd;
                     $command = new xo_shell_command($cmd);
                     if ( ! $command->execute())
