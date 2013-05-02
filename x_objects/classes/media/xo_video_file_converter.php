6fc9669d64e98f5bb137b2e75aba4b24c2d73b62
x-objects <david@reality-magic.com>
2013-05-02 13:59:39 -0500

    hot fix for ogg file extension

--- x_objects/classes/media/xo_video_file_converter.php
+++ x_objects/classes/media/xo_video_file_converter.php
@@ -45,7 +45,7 @@ class xo_video_file_converter {
                         ' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline '. $this->new_filename . ' 2>&1 ';
                     // if ogg...
                     if ($this->extension == 'mp4'){
-                        $new_filename = preg_replace('/\.mp4/','.ogg',$this->filename);
+                        $new_filename = preg_replace('/\.mp4/','.ogv',$this->filename);
                         $cmd = (string) $config->directory . ''."$cname $this->filename $new_filename 2>&1";
                     }
                     $this->command = $cmd;
