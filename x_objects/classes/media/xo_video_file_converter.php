e5d318d26c0af9c59713a465b56bf22bffbd33db
x-objects <david@reality-magic.com>
2013-05-28 10:23:30 -0500

    convert to 3 formats

--- x_objects/classes/media/xo_video_file_converter.php
+++ x_objects/classes/media/xo_video_file_converter.php
@@ -39,7 +39,8 @@ class xo_video_file_converter {
         else {
             // convert from AVI
             if ( in_array($this->extension,$this->extensions)){
-                // convert to Ogv and webm
+                // convert to MP4, Ogv and webm
+                $result &= $this->to_mp4();
                 $result &= $this->to_ogv();
                 $result &= $this->to_webm();
             }
@@ -51,26 +52,55 @@ class xo_video_file_converter {
      * Convert video to OGV (or to MP4)
      * @return bool true if successfully converted
      */
+    private function to_mp4(){
+        global $container;
+        $result = true;
+
+        if ($this->extension =='mp4')
+            return $result;
+        else{
+            // support to convert MP4 to Ogg
+            $new_extension = '.mp4';
+            $this->new_filenames['mp4'] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
+            if ( ! $this->config )
+                $result = false;
+            else {
+                $ext = $this->container->platform() == 'win'?".exe":'';
+                $cname = "ffmpeg$ext";
+                $cmd = (string) $this->config->directory . ''.$cname.' -i '.$this->filename.
+                    ' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline '. $this->new_filenames['ogv'] . ' 2>&1 ';
+
+                $this->commands['mp4'] = $cmd;
+                $command = new xo_shell_command($cmd);
+                if ( ! $command->execute())
+                    $this->error = $cname.'Could not run server video converter';
+                else {
+                    $result = true;
+                    $this->output = $command->output;
+                }
+            }
+            return $result;
+        }
+    }
+
+    /**
+     * Convert video to OGV (or to MP4)
+     * @return bool true if successfully converted
+     */
     private function to_ogv(){
         global $container;
 
         $result = true;
         // support to convert MP4 to Ogg
-        $new_extension = $this->extension == 'mp4'?'.ogv':'.mp4';
+        $new_extension = '.ogv';
         $this->new_filenames['ogv'] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
         if ( ! $this->config )
             $result = false;
         else {
             $ext = $this->container->platform() == 'win'?".exe":'';
             $cname = "ffmpeg$ext";
-            $cmd = (string) $this->config->directory . ''.$cname.' -i '.$this->filename.
-                ' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline '. $this->new_filenames['ogv'] . ' 2>&1 ';
-            /* if ogg...
-            if ($this->extension == 'mp4'){
-                $new_filenames['ogv'] = preg_replace('/\.mp4/','.ogv',$this->filename);
-                $cmd = (string) $this->config->directory . ''."$cname $this->filename $new_filename 2>&1";
-            }
-            */
+            $new_filenames['ogv'] = preg_replace('/\.mp4/','.ogv',$this->filename);
+            $cmd = (string) $this->config->directory . ''."$cname $this->filename ".$new_filenames['ogv']. " 2>&1";
             $this->commands['ogv'] = $cmd;
             $command = new xo_shell_command($cmd);
             if ( ! $command->execute())
