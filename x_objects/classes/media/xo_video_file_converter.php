3c3a2118b30b7c93c5a4e59b9aa4a57ece951713
x-objects <david@reality-magic.com>
2013-05-30 09:07:35 -0500

    refactor code, plus allow for deep verbose

--- x_objects/classes/media/xo_video_file_converter.php
+++ x_objects/classes/media/xo_video_file_converter.php
@@ -62,9 +62,9 @@ class xo_video_file_converter {
             if ( in_array(strtolower($this->extension),$this->extensions)){
                 if ($verbose) echo "$tag->event_format: $this->filename is a candidate for conversion\r\n";
                 // convert to MP4, Ogv and webm
-                if ( $bits & self::convert_mp4) $result &= $this->to_mp4();
-                if ( $bits & self::convert_ogv) $result &= $this->to_ogv();
-                if ( $bits & self::convert_webm) $result &= $this->to_webm();
+                if ( $bits & self::convert_mp4) $result &= $this->to_mp4($verbose);
+                if ( $bits & self::convert_ogv) $result &= $this->to_ogv($verbose);
+                if ( $bits & self::convert_webm) $result &= $this->to_webm($verbose);
             }
         }
         $success = $result?'success':'error';
@@ -77,25 +77,56 @@ class xo_video_file_converter {
      * Convert video to OGV (or to MP4)
      * @return bool true if successfully converted
      */
-    private function to_mp4(){
-        global $container;
-        $result = true;
+    private function to_mp4($verbose=false){ return $this->convert_to('mp4',$verbose); }
 
-        if (strtolower($this->extension) =='mp4')
+    /**
+     * Convert video to OGV (or to MP4)
+     * @return bool true if successfully converted
+     */
+    private function to_ogv($verbose=false){ return $this->convert_to('ogv',$verbose); }
+
+    /**
+     * Convert video to WebM
+     * @return bool true if successfully converted
+     */
+    private function to_webm($verbose=false){ return $this->convert_to('webm',$verbose); }
+
+    /**
+     * Convert to one of several formats
+     * @param $what string which format to convert to
+     * @param $verbose bool if true sends output to STDOUT
+     * @return bool true if successful
+     */
+    private function convert_to($what,$verbose=false){
+        $tag = new xo_codetag(xo_basename(__FILE__),__LINE__,get_class(),__FUNCTION__);
+        $args = array(
+            'mp4'=>' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline ',
+            'ogv'=>' ',
+            'webm'=>' '
+        );
+        $result = true;
+        // if already MP4 nothing to do for =>MP4 ;)
+        if ( $what == 'mp4' && strtolower($this->extension) =='mp4')
             return $result;
         else{
-            // support to convert MP4 to Ogg
-            $new_extension = '.mp4';
-            $this->new_filenames['mp4'] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
+            // extension is conversion type
+            $new_extension = ".$what";
+            // set new filename
+            $this->new_filenames[$what] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
+            // must have a configuration!
             if ( ! $this->config )
                 $result = false;
             else {
+                // allow to run on various platforms
                 $ext = $this->container->platform() == 'win'?".exe":'';
+                // get command name
                 $cname = "ffmpeg$ext";
-                $cmd = (string) $this->config->directory . ''.$cname.' -i '.$this->filename.
-                    ' -f mp4 -acodec aac -ac 2 -ar 44100 -b:a 128k -r 25 -b:v 512k -s 720x400 -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -strict -2 -level 30 -vprofile baseline '. $this->new_filenames['mp4'] . ' 2>&1 ';
-
-                $this->commands['mp4'] = $cmd;
+                // construct command for this conversion
+                $cmd = (string) $this->config->directory . ' '.$cname.' -i '.$this->filename. $args[$what]
+                    . $this->new_filenames[$what] . ' 2>&1 ';
+                // save it
+                $this->commands[$what] = $cmd;
+                if ($verbose) echo "$tag->event_format: converting to $what using $cmd\r\n";
                 $command = new xo_shell_command($cmd);
                 if ( ! $command->execute())
                     $this->error = $cname.'Could not run server video converter';
@@ -106,65 +137,5 @@ class xo_video_file_converter {
             }
             return $result;
         }
-    }
-
-    /**
-     * Convert video to OGV (or to MP4)
-     * @return bool true if successfully converted
-     */
-    private function to_ogv(){
-        global $container;
-
-        $result = true;
-        // support to convert MP4 to Ogg
-        $new_extension = '.ogv';
-        $this->new_filenames['ogv'] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
-        if ( ! $this->config )
-            $result = false;
-        else {
-            $ext = $this->container->platform() == 'win'?".exe":'';
-            $cname = "ffmpeg$ext";
-            $cmd = (string) $this->config->directory . ''."$cname -i $this->filename ".$this->new_filenames['ogv']. " 2>&1";
-            $this->commands['ogv'] = $cmd;
-            $command = new xo_shell_command($cmd);
-            if ( $this->logger) $this->logger->log("Converting to OGV cmd= $cmd new filename ".$this->new_filenames['ogv'],1,new xo_codetag(xo_basename(__FILE__),__LINE__,get_class(),__FUNCTION__));
-            if ( ! $command->execute())
-                $this->error = $cname.'Could not run server video converter';
-            else {
-                $result = true;
-                $this->output = $command->output;
-            }
-        }
-        return $result;
-    }
-
-    /**
-     * Convert video to WebM
-     * @return bool true if successfully converted
-     */
-    private function to_webm(){
-        $result = true;
-        // support to convert MP4 to Ogg
-        $new_extension = '.webm';
-        $this->new_filenames['webm'] = preg_replace("/\.$this->extension/",$new_extension,$this->filename);
-        if ( ! $this->config )
-            $result = false;
-        else {
-            $ext = $this->container->platform() == 'win'?".exe":'';
-            $cname = "ffmpeg$ext";
-            $cmd = (string) $this->config->directory . ''.$cname.' -i '.$this->filename.
-                '  '. $this->new_filenames['webm'] . ' 2>&1 ';
-            $this->commands['webm'] = $cmd;
-            if ( $this->logger) $this->logger->log("Converting to webM cmd= $cmd new filename ".$this->new_filenames['webm'],1,new xo_codetag(xo_basename(__FILE__),__LINE__,get_class(),__FUNCTION__));
-            $command = new xo_shell_command($cmd);
-            if ( ! $command->execute())
-                $this->error = 'Could not run server video converter';
-            else {
-                $result = true;
-                $this->output .= "\r\n\r\n".$command->output;
-            }
-        }
-        return $result;
-
     }
 }
