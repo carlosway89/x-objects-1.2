215baf4c765af91d1efe83ea523532c1d257d058
X-Objects Foundation <code@x-objects.org>
2013-07-07 13:35:02 -0500

    Fixed an issue with regex for matching member names within the email template

--- x_objects/classes/messaging/xo_email_template.php
+++ x_objects/classes/messaging/xo_email_template.php
@@ -1,14 +1,24 @@
 <?php
 /**
+ * Allows you to quickly templatize some HTML or text (from the database),
+ * together with a Business Object, thus accessing its members, and passing
+ * along the resulting Object as a parsed string
+ *
+ * Best used passed as an argument to xo_email_message
+ *
  * User: "David Owen Greenberg" <code@x-objects.org>
  * Date: 20/11/12
  * Time: 06:11 PM
  */
 class xo_email_template extends magic_object{
+    /**
+     * @param $key string lookup key in email_templates db table
+     * @param $bo business_object to use for fetching member values
+     */
     public function __construct($key,$bo){
         $this->template = new bo_email_template("key='$key'");
         $this->text = $this->template->text;
-        if ( preg_match_all("/#([a-z|_]+)/",$this->text,$hits)){
+        if ( preg_match_all("/#([a-z|A-Z|0-9|_]+)/",$this->text,$hits)){
            // print_r($hits);
             foreach ($hits[1] as $member){
                 $this->text= preg_replace("/#$member/",$bo->$member,$this->text);
